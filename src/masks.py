b1f05134eee2f6756f98844cb88d5f3eaa9e4fac
Roman Medvedev <roma.medvedev@mail.ru>
2024-12-23 09:05:52 +0300

    add loggers to utils, masks

--- src/masks.py
+++ src/masks.py
@@ -1,4 +1,16 @@
+import logging
+import os.path
+
+from src.config import APP_PATH
 from typing import Union
+
+logger = logging.getLogger('masks')
+log_filepath = os.path.join(APP_PATH, 'logs', 'masks.log')
+file_handler = logging.FileHandler(log_filepath, encoding='utf-8', mode='w')
+file_formatter =logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def get_masks_card_number(card_number: Union[str, int]) -> str:
@@ -11,10 +23,13 @@ def get_masks_card_number(card_number: Union[str, int]) -> str:
     входной аргумент (номер карты) 7000792289606361
     возвращаемый маскированный номер карты: 7000 79** **** 6361
     """
+    logger.info(f'Запуск функции get_masks_card_number с параметрами: card_number = {card_number}')
     if type(card_number) not in [str, int]:
+        logger.error('Неверный тип данных номера карты')
         raise TypeError("Неверный тип данных")
     card_number = str(card_number)
     if len(card_number) != 16 or card_number.isdigit() is False:
+        logger.error('Неверный формат номера карты (не более 16 символов и только числа')
         raise ValueError("Неверный формат номера карты")
     card_number_mask = card_number.replace(card_number[6:12], "*" * 6)
     card_number_mask = (
@@ -26,6 +41,7 @@ def get_masks_card_number(card_number: Union[str, int]) -> str:
         + " "
         + card_number_mask[12:]
     )
+    logger.info(f'Возвращаем маскированный номер карты клиента card_number_mask = {card_number_mask}')
     return card_number_mask
 
 
@@ -38,9 +54,22 @@ def get_masks_account(account_number: Union[str, int]) -> str:
     входной аргумент (номер счета) 73654108430135874305
     возвращаемый маскированный номер счета: **4305
     """
+    logger.info(f'Запуск функции get_masks_account с параметрами: account_number = {account_number}')
+    masked_account_number = ''
     if type(account_number) not in [str, int]:
+        logger.error('Неверный тип данных номера счета')
         raise TypeError("Неверный тип данных")
     account_number = str(account_number)
     if account_number.isdigit() is False:
+        logger.error('Неверный формат номера счета')
         raise ValueError("Неверный формат номера счета")
-    return "**" + account_number[-4:]
+    masked_account_number = "**" + account_number[-4:]
+    logger.info(f'Возвращаем маскированный номер счета masked_account_number = {masked_account_number}')
+    return masked_account_number
+
+if __name__ == '__main__':
+    print(get_masks_card_number('7000792289606361'))
+    #print(get_masks_card_number('70007922896'))
+    #print(get_masks_card_number('A000792289606361'))
+
+    print(get_masks_account('367968696986554546899845'))
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,17 @@
 import json
+import logging
 import os.path
 
 from src.config import APP_PATH
+
+
+logger = logging.getLogger('utils')
+log_filepath = os.path.join(APP_PATH, 'logs', 'utils.log')
+file_handler = logging.FileHandler(log_filepath, encoding='utf-8', mode='w')
+file_formatter =logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
 
 
 def get_operation(json_path: str = None) -> list[dict]:
@@ -9,17 +19,32 @@ def get_operation(json_path: str = None) -> list[dict]:
     :param json_path: - путь к json-файлу
     :return: список словарей, представляющих собой транзакции
     '''
+    logger.info(f'Запуск функции get_operation c параметрами json_path = {json_path}')
     operations = []
+    logger.info('Проверка наличия пути к json-файлу в качестве аргумента.')
     if json_path:
+        logger.info('Путь к json передан. Формирование полного абсолютного пути к json-файлу')
         json_filepath = os.path.join(APP_PATH, os.path.normpath(json_path))
-        print(json_filepath)
+        logger.info('Проверка валидности пути к json-файлу')
         if os.path.exists(json_filepath):
+            logger.info('Путь к json валидный')
             try:
+                logger.info('Попытка открытия и чтения json-файла')
                 with open(json_filepath, encoding='utf-8') as f:
                     if f:
+                        logger.info('json-файл не пустой. Чтение json')
                         operations = json.load(f)
                     if not isinstance(operations, list):
-                        operations = []
+                        logger.error('Файл содержит не список. Возврат пустого списка')
+                        return []
+                    logger.info('Возврат списка словарей')
+                    return operations
             except:
-                operations = []
-    return operations
+                logger.error('Ошибка чтения файла. Возврат пустого списка транзакций')
+                return []
+        else:
+            logger.error('Путь к файлу невалидный. Возврат пустого списка')
+            return []
+    else:
+        logger.error('Путь к json-файлу в качестве аргумента не передан. Возврат пустого списка транзакций')
+        return []
